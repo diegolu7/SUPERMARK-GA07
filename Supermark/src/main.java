61606ebb74f979249067c14f3022dacb51f65d8d
diegolu7 <diegolu7@gmail.com>
2022-07-03 22:00:48 -0300

    creating main class + login class

---
+++ Supermark/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		//programamos
+		//Menu IF(ADMIN) / USUARIO
+		
+		//1 -> ¿ADMIN O USUARIO NORMAL?
+		//2 -> USUARIO NORMAL : (LOGEARSE/REGISTRARSE)
+		//3 si Logeado -> instanciamos USUARIO
+		
+		//
+	} 
+
+}
