d192a67c484592072096e5dd6b60872c6ccb3af3
diegolu7 <diegolu7@gmail.com>
2022-07-03 22:02:06 -0300

    class login

--- Supermark/src/main.java
+++ Supermark/src/main.java
@@ -4,6 +4,10 @@ public class main {
 	public static void main(String[] args) {
 		//programamos
 		//Menu IF(ADMIN) / USUARIO
+		//do{    
+			//code to be executed / loop body  
+			//update statement   
+			//}while (condition);    
 		
 		//1 -> ¿ADMIN O USUARIO NORMAL?
 		//2 -> USUARIO NORMAL : (LOGEARSE/REGISTRARSE)
