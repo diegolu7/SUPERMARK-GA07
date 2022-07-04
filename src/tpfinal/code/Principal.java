9e19885029bed1238f0bdcb998e7d80ddd20bf74
sarainesgonz <gonzalezsaritaines@gmail.com>
2022-07-04 00:08:39 -0300

    segundo commit

--- src/tpfinal/code/Principal.java
+++ src/tpfinal/code/Principal.java
@@ -8,12 +8,12 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) throws SQLException {
-//		Scanner teclado = new Scanner(System.in);
-//		System.out.println("Escriba el nombre del producto que busca: ");
-//		String nombreProducto = teclado.next();
-//		Producto producto = new Producto(0, nombreProducto, 0, null, 0, null);
-//		teclado.close();
-//		producto.consultarProducto();
+	Scanner teclado = new Scanner(System.in);
+		System.out.println("Escriba el nombre del producto que busca: ");
+		String nombreProducto = teclado.next();
+		Producto producto = new Producto(0, nombreProducto, 0, null, 0, null);
+		teclado.close();
+		producto.consultarProducto();
 		
 		
 		Scanner teclado1 = new Scanner (System.in);	
